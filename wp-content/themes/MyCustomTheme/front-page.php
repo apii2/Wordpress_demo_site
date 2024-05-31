a0b986b4bd6791e23d7cfd499cdb3d68b7afed37
apeksha <apekshrestha22@gmail.com>
2024-05-31 18:10:31 +0545

    frontpage modified

--- wp-content/themes/MyCustomTheme/front-page.php
+++ wp-content/themes/MyCustomTheme/front-page.php
@@ -15,6 +15,204 @@ get_header();
   </div>
 </section>
 
+<main id="main">
+
+  <!-- Featured Services Section -->
+  <section id="featured-services" class="featured-services">
+    <div class="container">
+
+      <div class="row gy-4">
+
+        <div class="col-xl-3 col-md-6 d-flex" data-aos="zoom-out">
+          <div class="service-item position-relative">
+            <div class="icon"><i class="bi bi-book"></i></div>
+            <h4><a href="" class="stretched-link">Structured Learning</a></h4>
+            <p>We offer structured courses designed by experts, guiding learners from basics to advanced topics in a logical sequence.</p>
+          </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 d-flex" data-aos="zoom-out" data-aos-delay="200">
+          <div class="service-item position-relative">
+            <div class="icon"><i class="bi bi-activity"></i></div>
+            <h4><a href="" class="stretched-link">Interactive Exercises</a></h4>
+            <p>We provide interactive exercises that allow learners to practice coding with their understanding of concepts.</p>
+          </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 d-flex" data-aos="zoom-out" data-aos-delay="400">
+          <div class="service-item position-relative">
+            <div class="icon"><i class="bi bi-bounding-box-circles icon"></i></div>
+            <h4><a href="" class="stretched-link">Community Support</a></h4>
+            <p>Our platform provide active communities where learners can seek help and share insights creating a supportive environment for learning.</p>
+          </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 d-flex" data-aos="zoom-out" data-aos-delay="600">
+          <div class="service-item position-relative">
+            <div class="icon"><i class="bi bi-universal-access-circle"></i></div>
+            <h4><a href="" class="stretched-link">Accessibility</a></h4>
+            <p>Learners can access our platform anytime, anywhere, making it convenient for them to study at their own pace and schedule.</p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+
+  <!-- ======= Call To Action Section ======= -->
+  <section id="cta" class="cta">
+    <div class="container" data-aos="zoom-out">
+
+      <div class="row g-5">
+
+        <div class="col-lg-8 col-md-6 content d-flex flex-column justify-content-center order-last order-md-first">
+          <h3>Coding isn't just a <em>Passion</em>; it's a lifestyle.</h3>
+          <p>It's the language of innovation, where every line of code tells a story of creativity and problem-solving. Welcome to a world where logic meets imagination, and every keystroke shapes the future.</p>
+          <a class="cta-btn align-self-start" href="#">Start Now</a>
+        </div>
+
+        <div class="col-lg-4 col-md-6 order-first order-md-last d-flex align-items-center">
+          <div class="img">
+            <img src="../../wp-content/uploads/2024/05/bggg.jpg" alt="" class="img-fluid">
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+
+  <!-- ======= On Focus Section ======= -->
+  <section id="onfocus" class="onfocus">
+    <div class="container-fluid p-0" data-aos="fade-up">
+
+      <div class="row g-0">
+        <div class="content d-flex flex-column justify-content-center h-100">
+          <h3>Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program.</h3>
+          <p class="fst-italic">- Linus Torvalds</p>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <!-- ======= About Section ======= -->
+  <section id="about" class="about">
+    <div class="container" data-aos="fade-up">
+
+      <div class="section-header">
+        <h2>Our Blogs</h2>
+      </div>
+
+      <div class="row g-4 g-lg-5" data-aos="fade-up" data-aos-delay="200">
+
+        <div class="col-lg-5">
+          <div class="about-img">
+            <img src="../../wp-content/uploads/2024/05/lang.jpg" class="img-fluid" alt="">
+          </div>
+        </div>
+
+        <div class="col-lg-7 blog-content">
+          <div>
+            <p>
+              Discover a wealth of knowledge and insights through our blog series on programming languages. Whether you're a seasoned developer or a beginner, our articles cover a wide range of topics from the latest trends and best practices to in-depth tutorials and expert tips. Stay updated with comprehensive guides on popular languages like Python, JavaScript, and Php, as well as emerging technologies shaping the future of software development.
+            </p>
+            <a class="blog-btn align-self-start" href="#">Read Our Blogs<i class="bi bi-arrow-right"></i></a>
+          </div>
+
+
+          <!-- Tabs
+          <ul class="nav nav-pills mb-3">
+            <li><a class="nav-link active" data-bs-toggle="pill" href="#tab1">Saepe fuga</a></li>
+            <li><a class="nav-link" data-bs-toggle="pill" href="#tab2">Voluptates</a></li>
+            <li><a class="nav-link" data-bs-toggle="pill" href="#tab3">Corrupti</a></li>
+          </ul>End Tabs
+
+          Tab Content
+          <div class="tab-content">
+
+            <div class="tab-pane fade show active" id="tab1">
+
+              <p class="fst-italic">Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
+
+              <div class="d-flex align-items-center mt-4">
+                <i class="bi bi-check2"></i>
+                <h4>Repudiandae rerum velit modi et officia quasi facilis</h4>
+              </div>
+              <p>Laborum omnis voluptates voluptas qui sit aliquam blanditiis. Sapiente minima commodi dolorum non eveniet magni quaerat nemo et.</p>
+
+              <div class="d-flex align-items-center mt-4">
+                <i class="bi bi-check2"></i>
+                <h4>Incidunt non veritatis illum ea ut nisi</h4>
+              </div>
+              <p>Non quod totam minus repellendus autem sint velit. Rerum debitis facere soluta tenetur. Iure molestiae assumenda sunt qui inventore eligendi voluptates nisi at. Dolorem quo tempora. Quia et perferendis.</p>
+
+              <div class="d-flex align-items-center mt-4">
+                <i class="bi bi-check2"></i>
+                <h4>Omnis ab quia nemo dignissimos rem eum quos..</h4>
+              </div>
+              <p>Eius alias aut cupiditate. Dolor voluptates animi ut blanditiis quos nam. Magnam officia aut ut alias quo explicabo ullam esse. Sunt magnam et dolorem eaque magnam odit enim quaerat. Vero error error voluptatem eum.</p>
+
+            </div>End Tab 1 Content
+
+            <div class="tab-pane fade show" id="tab2">
+
+              <p class="fst-italic">Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
+
+              <div class="d-flex align-items-center mt-4">
+                <i class="bi bi-check2"></i>
+                <h4>Repudiandae rerum velit modi et officia quasi facilis</h4>
+              </div>
+              <p>Laborum omnis voluptates voluptas qui sit aliquam blanditiis. Sapiente minima commodi dolorum non eveniet magni quaerat nemo et.</p>
+
+              <div class="d-flex align-items-center mt-4">
+                <i class="bi bi-check2"></i>
+                <h4>Incidunt non veritatis illum ea ut nisi</h4>
+              </div>
+              <p>Non quod totam minus repellendus autem sint velit. Rerum debitis facere soluta tenetur. Iure molestiae assumenda sunt qui inventore eligendi voluptates nisi at. Dolorem quo tempora. Quia et perferendis.</p>
+
+              <div class="d-flex align-items-center mt-4">
+                <i class="bi bi-check2"></i>
+                <h4>Omnis ab quia nemo dignissimos rem eum quos..</h4>
+              </div>
+              <p>Eius alias aut cupiditate. Dolor voluptates animi ut blanditiis quos nam. Magnam officia aut ut alias quo explicabo ullam esse. Sunt magnam et dolorem eaque magnam odit enim quaerat. Vero error error voluptatem eum.</p>
+
+            </div>End Tab 2 Content
+
+            <div class="tab-pane fade show" id="tab3">
+
+              <p class="fst-italic">Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
+
+              <div class="d-flex align-items-center mt-4">
+                <i class="bi bi-check2"></i>
+                <h4>Repudiandae rerum velit modi et officia quasi facilis</h4>
+              </div>
+              <p>Laborum omnis voluptates voluptas qui sit aliquam blanditiis. Sapiente minima commodi dolorum non eveniet magni quaerat nemo et.</p>
+
+              <div class="d-flex align-items-center mt-4">
+                <i class="bi bi-check2"></i>
+                <h4>Incidunt non veritatis illum ea ut nisi</h4>
+              </div>
+              <p>Non quod totam minus repellendus autem sint velit. Rerum debitis facere soluta tenetur. Iure molestiae assumenda sunt qui inventore eligendi voluptates nisi at. Dolorem quo tempora. Quia et perferendis.</p>
+
+              <div class="d-flex align-items-center mt-4">
+                <i class="bi bi-check2"></i>
+                <h4>Omnis ab quia nemo dignissimos rem eum quos..</h4>
+              </div>
+              <p>Eius alias aut cupiditate. Dolor voluptates animi ut blanditiis quos nam. Magnam officia aut ut alias quo explicabo ullam esse. Sunt magnam et dolorem eaque magnam odit enim quaerat. Vero error error voluptatem eum.</p>
+
+            </div>End Tab 3 Content -->
+
+          </div>
+
+        </div> 
+
+      </div>
+
+    </div>
+  </section>
+</main>
 
 <?php
 get_footer();
